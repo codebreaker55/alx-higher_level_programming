cf30b88feb95e1cf25210ed0344c8cbaaebfc9d0
belal <belalismail252@gmail.com>
2024-01-02 11:34:51 -0800

    the less material you need task

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for n1 in range(10):
+    for n2 in range(n1, 10):
+        if n1 < n2:
+            print("{:d}{:d}".format(n1,n2),
+                end=", " if n1 != 8 and n2 != 9 else "\n")
