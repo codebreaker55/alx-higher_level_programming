2b9f91934983391beae0063b5f0d294cdff60c8a
belal <belalismail252@gmail.com>
2024-01-02 16:19:34 -0800

    mirror task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alph in range(25, -1, -1):
-    ch = alph + ord('A')
+    ch = alph + ord('a')
     if alph % 2 == 0:
-        ch = ch + 32
+        ch = ch - 32
     print("{:c}".format(ch), end="")
