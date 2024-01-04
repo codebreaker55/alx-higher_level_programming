4a49507dcba2b93ea181d48821c59ce22fa9222e
belal <belalismail252@gmail.com>
2024-01-04 15:24:04 -0800

    Build my own calculator! Task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
         exit(0)
     elif oper == "*":
-        print("{} * {} = {}".format(a, b, muul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
         exit(0)
     elif oper == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
