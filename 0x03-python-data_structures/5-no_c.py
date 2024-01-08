464ca483fe0b538e985a7ef2b9feb927d9876771
belal <belalismail252@gmail.com>
2024-01-07 16:17:56 -0800

    now you can c me task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     strin = ""
-    for ch in range(len(my_string)):
-        if (my_string[ch] != 'c' and my_string[ch] != 'C'):
-            strin = strin + my_string[ch]
+    for ch in my_string:
+        if (ch != 'c' and ch != 'C'):
+            strin = strin + ch
     return strin
 
