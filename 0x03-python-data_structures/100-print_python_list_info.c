728c9dcdf95906bff275898d5ced9cbf31ee9f89
belal <belalismail252@gmail.com>
2024-01-08 03:55:16 -0800

    CPython #0 task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#include <python.h>
+
+/**
+ * print_python_list_info - function that prints basic info of python lists
+ *
+ * @p: is a pyobject list
+ *
+ * Return: nothing
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	int siz, aloc, n;
+	PyObject *ob;
+
+	siz = Py_SIZE(p);
+	aloc = ((PyListobject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", siz);
+	printf("[*] Allocated = %d\n", aloc);
+
+	for (n = 0; n < siz; n++)
+	{
+		printf("Element %d: ", n);
+
+		ob = PyList_GetItem(p, n);
+		printf("%s\n", Py_TYPE(ob)->tp_name);
+	}
+}
