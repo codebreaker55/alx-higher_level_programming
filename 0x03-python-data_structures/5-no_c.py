fbdfe3b37758ec85e987b2d7b3a8340f6fb42bce
belal <belalismail252@gmail.com>
2024-01-07 16:02:50 -0800

    Can you c me Task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     strin = ""
     for ch in range(len(my_string)):
-        if my_string[ch] != 'c' and my_string[ch] != 'C':
+        if (my_string[ch] != 'c' and my_string[ch] != 'C'):
             strin = strin + my_string[ch]
     return strin
 
