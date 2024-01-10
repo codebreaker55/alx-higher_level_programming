24b322304502e2ab15f4b1958faa6b415da4f624
belal <belalismail252@gmail.com>
2024-01-10 04:00:12 -0800

    squared simple task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda sub_mtrx: list(map(lambda n: n ** 2, sub_mtrx)), matrix))
+    return list(map(lambda submtrx: list(map(lambda n: n ** 2, submtrx)), matrix))
