9acf8a7ecc89547a6519cf0c050f52a249897f32
belal <belalismail252@gmail.com>
2024-01-10 16:00:25 -0800

    Roman to integer task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,5 +7,5 @@ def roman_to_int(roman_string):
     dig = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for rom in reversed(roman_string):
         num = dig[rom]
-        tot = tot + num if tot < num * 5 else -num
+        tot += num if tot < num * 5 else -num
     return tot
