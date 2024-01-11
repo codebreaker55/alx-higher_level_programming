0f2aa1a5d4df6c2519df2d9b6a2184b39309a820
belal <belalismail252@gmail.com>
2024-01-10 20:00:53 -0800

    python task

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,51 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_list_info - function that prints basic info of python lists
+ *
+ * @p: is a pyobject list
+ *
+ * Return: nothing
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	int siz, aloc, n;
+	const char * type;
+	PyListObject *lis = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
+
+	siz = var->ob_size;
+	aloc = lis->allocated;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %d\n", siz);
+	printf("[*] Allocated = %d\n", aloc);
+	
+	for (n = 0; n < siz; n++)
+	{
+		type = lis->ob_item[n]->ob_type->tp_name;
+		printf("Element %d: %s\n", n, type);
+
+		if (strcmp(type, "bytes") == 0)
+		{
+			print_python_bytes(lis->ob_item[n]);
+		}
+	}
+}
+
+/**
+ * print_python_bytes - function that prints basic info of python objects
+ *
+ * @p: is a pyobject list
+ *
+ * Return: void
+*/
+
+void print_python_bytes(PyObject *p)
+{
+	unsigned char n, size;
+	PyBytesObject *byts = (PyBytesObject *)p;
