eddeb3c412306eb33e3e00ae1f2b78abedcc062d
belal <belalismail252@gmail.com>
2024-01-22 02:36:11 -0800

    task fix try

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,7 @@ def safe_print_integer_err(value):
     int_val = True
     try:
         print("{:d}".format(value))
-    except Exception as err:
+    except ValueError as err:
         print("Exception:", err, file =sys.stderr)
         int_val = False
     return int_val
