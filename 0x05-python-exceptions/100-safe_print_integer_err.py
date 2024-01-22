85e47586a57877fe29b16452ce25211f5c7f5a7e
belal <belalismail252@gmail.com>
2024-01-22 02:22:17 -0800

    integer error task

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    int_val = True
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        print("Exception:", err, file =sys.stderr)
+        int_val = False
+    return int_val
