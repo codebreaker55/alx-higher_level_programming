46cfc91b0114b7a4bc1b9feab7fc93b70ece2b13
belal <belalismail252@gmail.com>
2024-01-22 02:38:12 -0800

    Task error integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,12 @@ import sys
 
 
 
+
 def safe_print_integer_err(value):
     int_val = True
     try:
         print("{:d}".format(value))
-    except ValueError as err:
+    except Exception as err:
         print("Exception:", err, file =sys.stderr)
         int_val = False
     return int_val
