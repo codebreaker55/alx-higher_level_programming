40bc79b3f84d55fe94b8bf7e9c950eba1fc3f1c8
belal <belalismail252@gmail.com>
2024-01-23 03:20:13 -0800

    size for the square class task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,5 +3,9 @@
 
 
 class Square:
+    """defining the size for the square class."""
+
     def __init__(self, size):
+        """set a Private instance attribute for size."""
+
         self.__size = size
