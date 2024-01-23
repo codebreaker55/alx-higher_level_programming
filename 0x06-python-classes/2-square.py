b5f2a1aa2ac06852122ca0edf271b5891d11da67
belal <belalismail252@gmail.com>
2024-01-23 03:44:19 -0800

    size validation task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Initializing a class"""
+
+
+class Square:
+    """Defining the size within the square class"""
+
+    def __init__(self, size=0):
+        """Inizializing the conditions for Private instance attribute: size"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
