126830fa05c9355b4f4f4cea9f2915771cd7eb77
belal <belalismail252@gmail.com>
2024-01-23 17:06:55 -0800

    Magic Class Task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -13,15 +13,15 @@ class MagicClass:
         self.__radius = 0
 
         if type(radius) is not float and type(radius) is not int:
-            raise TypeError("radius must be a number")
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
-        def area(self):
-            """used to return the magic class area"""
+    def area(self):
+        """used to return the magic class area"""
 
-            return self.__radius * self.__radius * math.pi
+        return self.__radius * self.__radius * math.pi
 
-        def circumference(self):
-            """it returns the circumference of the magic class"""
+    def circumference(self):
+        """it returns the circumference of the magic class"""
 
-            return self.__radius * 2 * math.pi
+        return self.__radius * 2 * math.pi
