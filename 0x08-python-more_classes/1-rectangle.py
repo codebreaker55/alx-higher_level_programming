48db1f16037b01e94ae3d59b1609107c581dcd63
belal <belalismail252@gmail.com>
2024-01-29 03:38:27 -0800

    pycodestyle fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,13 +11,11 @@ class Rectangle:
         self.width = width
         self.height = height
 
-
     @property
     def width(self):
         """using getter to retrieve the width"""
 
         return self.__width
-
 
     @width.setter
     def width(self, value):
@@ -28,13 +26,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-
     @property
     def height(self):
         """using getter to retrieve the height"""
 
         return self.__height
-
 
     @height.setter
     def height(self, value):
