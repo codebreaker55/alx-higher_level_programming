14f822b78d71eed57fd53bf27dc9a7a77c8abebc
belal <belalismail252@gmail.com>
2024-01-29 03:31:07 -0800

    Rectangle 1 task

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,41 +8,40 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """defining width and height"""
 
-        self.height = height
         self.width = width
+        self.height = height
 
 
-@property
-def width(self):
-    """using getter to retrieve the width"""
+    @property
+    def width(self):
+        """using getter to retrieve the width"""
 
-    return self.__width
-
-
-@width.setter
-def width(self, value):
-    """using setter to set exceptions"""
-
-    if type(value) is not int:
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self.__width = value
+        return self.__width
 
 
-@property
-def height(self):
-    """using getter to retrieve the height"""
+    @width.setter
+    def width(self, value):
+        """using setter to set exceptions"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-    return self.__height
+
+        @property
+        def height(self):
+            """using getter to retrieve the height"""
+
+            return self.__height
 
 
-@height.setter
-def height(self, value):
-    """using setter to set exceptions"""
+        @height.setter
+        def height(self, value):
+            """using setter to set exceptions"""
 
-    if type(value) is not int:
-        raise TypeError("height must be an integer")
-    if value < 0:
-        raise ValueError("height must be >= 0")
-    self.__height = value
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = value
