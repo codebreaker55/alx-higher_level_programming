cb605a6e879322eda9ebe4f5eb097a371b4be90a
belal <belalismail252@gmail.com>
2024-01-30 15:43:06 -0800

    new try for low memory task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,4 +6,4 @@ class LockedClass:
     """that prevents the user from dynamically creating new instance attributes
     except if the new instance attribute is called first_name"""
 
-    __name__ = ("first_name")
+    __slot__ = ("first_name")
