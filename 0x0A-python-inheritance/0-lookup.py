0d708eda3daae4898e05625300d3325b16c0ce6a
belal <belalismail252@gmail.com>
2024-02-05 03:18:45 -0800

    lookup task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""lookup function module"""
+
+
+def lookup(obj):
+    """ function that returns the list of available attributes
+        and methods of an object
+        Args:
+            obj: is the object to be listed
+
+        Returns:
+                a list object
+    """
+
+
+return dir(obj)
