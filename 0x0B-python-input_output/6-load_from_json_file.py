5b2f6b4eca936a874b8b41794741b7c3d4538c8f
belal <belalismail252@gmail.com>
2024-02-07 11:53:13 -0800

    json file object creation task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 def load_from_json_file(filename):
     """function that creates an Object from a “JSON file”"""
 
-    with open(fileaname, "r", encoding="utf-8") as fil:
+    with open(filename, "r", encoding="utf-8") as fil:
         return json.load(fil)
