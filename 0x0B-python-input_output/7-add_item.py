d2262ae459422c951b8d17c92a142547fdf15675
belal <belalismail252@gmail.com>
2024-02-07 12:56:52 -0800

    new try to add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """json load, add, save module"""
 import sys
-
-
 save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file.py").load_from_json_file
 
