89c58fd2446986e744d0d61e1b81a9bc2f001639
belal <belalismail252@gmail.com>
2024-02-07 12:41:39 -0800

    task load, add, and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,12 @@ import sys
 save_to_json_file = __import__(5-save_to_json_file.py).save_to_json_file
 load_from_json_file = __import__(6-load_from_json_file.py).load_from_json_file
 
-arg_list = list(sys.argv[1:])
+arglist = list(sys.argv[1:])
 
 try:
     old_list = load_from_json_file("add_item.json")
 except Exception:
     old_list = []
 
-old_list.extend(arg_list)
+old_list.extend(arglist)
 save_to_json_file(old_list, "add_item.json")
