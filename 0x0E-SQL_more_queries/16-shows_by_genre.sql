4ea0de85efcdbaf443c90b282b69e46b689cf77d
belal <belalismail252@gmail.com>
2024-03-13 18:03:06 -0700

    what a show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -5,8 +5,7 @@
 -- use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELECT title, name
-FROM tv_shows
+SELECT title, name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title ASC, name ASC;
