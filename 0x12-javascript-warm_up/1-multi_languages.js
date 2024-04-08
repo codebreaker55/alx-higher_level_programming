3495922cce28201e47a542617e8f0bd3eec2abfe
belal <belalismail252@gmail.com>
2024-04-08 09:06:11 -0700

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -7,9 +7,9 @@
  * must use console.log(...) to print all output
  * not allowed to use var
 */
-const line_1 = 'C is fun';
-const line_2 = 'Python is cool';
-const line_3 = 'JavaScript is amazing';
-console.log(line_1);
-console.log(line_2);
-console.log(line_3);
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+console.log(line1);
+console.log(line2);
+console.log(line3);
