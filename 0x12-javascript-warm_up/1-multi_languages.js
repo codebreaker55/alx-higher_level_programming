e08695824dc329e9a5ba146179d98408dd1868db
belal <belalismail252@gmail.com>
2024-04-08 09:02:09 -0700

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+ * a script that prints 3 lines:
+ * The first line: “C is fun”
+ * The second line: “Python is cool”
+ * The third line: “JavaScript is amazing”
+ * must use console.log(...) to print all output
+ * not allowed to use var
+*/
+const line_1 = 'C is fun';
+const line_2 = 'Python is cool';
+const line_3 = 'JavaScript is amazing';
+console.log(line_1);
+console.log(line_2);
+console.log(line_3);
