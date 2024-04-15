67c26509ee90d4ef4441ce3c5eb9514731abf1cb
belal <belalismail252@gmail.com>
2024-04-16 02:36:28 +0300

    model city

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,15 +4,14 @@ import sys
 from model_city import City
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from model_state import State
+from model_state import Base, State
 
 
 if __name__ == "__main__":
     sav = sys.argv
     # connect to a MySQL server running on localhost at port 3306
     s_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(s_str.format(sav[1], sav[2], sav[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(s_str.format(sav[1], sav[2], sav[3]))
     Session = sessionmaker(bind=engine)
 
     # create a session
