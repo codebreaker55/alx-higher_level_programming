82682ae51f090c516d7f773d0642e63549ca1200
belal <belalismail252@gmail.com>
2024-04-15 02:13:06 -0700

    model state

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
 """module that lists all State objects from the database hbtn_0e_6_usa"""
-import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
 
-sav = sys.argv
-if len(sav) < 4:
-    exit(1)
-# connect to a MySQL server running on localhost at port 3306
-s_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-engine = create_engine(s_str.format(sav[1], sav[2], sav[3]))
-Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
 
-# create tables if they do not exist
-Base.metadata.create_all(engine)
+    sav = sys.argv
+    if len(sav) < 4:
+        exit(1)
+    # connect to a MySQL server running on localhost at port 3306
+    s_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(s_str.format(sav[1], sav[2], sav[3]))
+    Session = sessionmaker(bind=engine)
 
-# create a session
-session = Session()
+    # create tables if they do not exist
+    Base.metadata.create_all(engine)
 
-# query and print State data
-output = session.query(State).order_by(State.id).all()
-for state in output:
-    print("{}: {}".format(state.id, state.name))
+    # create a session
+    session = Session()
 
-session.close()
+    # query and print State data
+    output = session.query(State).order_by(State.id).all()
+    for state in output:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
