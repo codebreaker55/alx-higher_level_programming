3b4edc95f9988176c43ad681b3a7ef8fde3fa3f8
belal <belalismail252@gmail.com>
2024-04-15 14:54:56 +0300

    new task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,6 @@
 """module that contains the class definition of a State
 and an instance Base = declarative_base()"""
 import sys
-from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,6 +21,6 @@ class City(Base):
         can’t be null and is a foreign key to states.id
     """
     __tablename__ = "cities"
-    id = Column(Integer, unique=True, nullaable=False, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
