d66238908fea79c36fcb9317c2a1bfe34c3d6413
belal <belalismail252@gmail.com>
2024-04-16 02:19:09 +0300

    relationship states

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     # connect to a MySQL server running on localhost at port 3306
     s_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
     engine = create_engine(s_str.format(sav[1], sav[2], sav[3]))
-    
+
     # create tables if they do not exist
     Base.metadata.create_all(engine)
     # create a session
