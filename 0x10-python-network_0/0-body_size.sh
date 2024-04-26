a29983e88155b34f95ba9de1fdf4c09166b1d3ba
belal <belalismail252@gmail.com>
2024-04-26 14:42:56 -0700

    body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# a Bash script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response
+# The size must be displayed in bytes, and have to use curl
+curl -s "$1" | wc -c
