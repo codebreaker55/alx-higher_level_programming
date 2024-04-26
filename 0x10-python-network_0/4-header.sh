f428c33524705f628c52975a6e7bd3ffedeb56c4
belal <belalismail252@gmail.com>
2024-04-26 15:19:40 -0700

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response and a header variable X-School-User-Id must be sent with the value 98, using curl
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response and a header variable X-School-User-Id
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
