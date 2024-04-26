40acb9c4b20a415e4eacde639850b0a580834e35
belal <belalismail252@gmail.com>
2024-04-26 15:08:26 -0700

    code style fix

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-# using curl
+# a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response, using curl
 curl -sX DELETE "$1"
