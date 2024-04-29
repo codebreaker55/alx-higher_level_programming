00448b8b74c36985dff412e061573be0154b11d0
belal <belalismail252@gmail.com>
2024-04-28 17:40:21 -0700

    a small fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,11 +9,11 @@ using the packages requests and sys
 no need to error check arguments passed to the script (number or type)
 """
 from sys import argv
-from  requests import post
+from requests import post
 
 
 if __name__ == "__main__":
     sys_url = argv[1]
     email = argv[2]
-    req = post(sys_url, {'email': email})
+    req = post(sys_url, data={'email': email})
     print(req.text)
