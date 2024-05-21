9347f31d44cffae109e71c10259e3a168f3140f5
belal <belalismail252@gmail.com>
2024-05-21 11:49:36 +0300

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that display the status code of a GET request
+
+const req = require('request');
+req.get(process.argv[2],
+  function (error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+    console.log('code:' + ' ' + response.statusCode);
+    }
+  });
