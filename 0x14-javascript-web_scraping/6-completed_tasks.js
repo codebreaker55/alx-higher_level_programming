cc77578db5482d3a0ee46d1132bdba02b050ef11
belal <belalismail252@gmail.com>
2024-05-21 13:09:09 +0300

    semistandard fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,18 +7,17 @@ const url = process.argv[2];
 req.get(url, { json: true }, function (error, response, body) {
   if (error) {
     console.log(error);
-    return;
   } else {
-  const completedTasks = {};
-  body.forEach((todo) => {
-    if (todo.completed) {
-      if (!completedTasks[todo.userId]) {
-        completedTasks[todo.userId] = 1;
-      } else {
-        completedTasks[todo.userId] += 1;
+    const completedTasks = {};
+    body.forEach((todo) => {
+      if (todo.completed) {
+        if (!completedTasks[todo.userId]) {
+          completedTasks[todo.userId] = 1;
+        } else {
+          completedTasks[todo.userId] += 1;
+        }
       }
-    }
-  });
-  console.log(completedTasks);
+    });
+    console.log(completedTasks);
   }
 });
