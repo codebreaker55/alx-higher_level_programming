221174846bbb92a209b2e1d7c95aab3ef9ba7a97
belal <belalismail252@gmail.com>
2024-05-21 13:49:37 +0300

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a script that prints all characters of a Star Wars movie
+//  a script that prints all characters of a Star Wars movie
 
 const req = require('request');
 const url = process.argv[2];
@@ -9,8 +9,8 @@ req.get(SW_API + url, function (error, response, body) {
     console.log(error);
   } else {
     const content = JSON.parse(body);
-    const charac = content.characters;
-    for (const ch of charac) {
+    const character = content.characters;
+    for (const ch of character) {
       req.get(ch, function (error, response, body) {
         if (error) {
           console.log(error);
